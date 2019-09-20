7c478bbac1bbe1718e0d127f9d3f9ff47e90d5dd
Joshua Tucker <thejltucker@gmail.com>
2019-09-20 16:37:04 -0400

    Added simple routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
 import {
   MatToolbarModule,
@@ -14,6 +15,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 
+const routes = [
+  { path: 'register', component: RegistrationComponent },
+  { path: '**', component: RegistrationComponent }
+]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,6 +30,7 @@ import { RegistrationComponent } from './components/registration/registration.co
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     ReactiveFormsModule,
     MatToolbarModule,
     MatButtonModule,
