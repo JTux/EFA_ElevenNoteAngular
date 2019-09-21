9d8d05025ab1e567813aab87a59048b9fb724945
Joshua Tucker <thejltucker@gmail.com>
2019-09-20 20:44:42 -0400

    Added Token and Login after Registration

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { RegisterUser } from '../models/RegisterUser';
 import { HttpClient } from '@angular/common/http';
+import { Token } from '../models/Token';
 
-const Api_Url = 'https://localhost:44383/';
+const Api_Url = 'https://localhost:44383';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,13 @@ export class AuthService {
   constructor(private _http: HttpClient) { }
 
   register(regUserData: RegisterUser) {
-    return this._http.post(`${Api_Url}api/Account/Register`, regUserData);
+    return this._http.post(`${Api_Url}/api/Account/Register`, regUserData);
+  }
+
+  login(loginInfo) {
+    const str = `grant_type=password&username=${encodeURI(loginInfo.email)}&password=${encodeURI(loginInfo.password)}`;
+    return this._http.post(`${Api_Url}/token`, str).subscribe((token: Token) => {
+      localStorage.setItem('id_token', token.access_token);
+    });
   }
 }
