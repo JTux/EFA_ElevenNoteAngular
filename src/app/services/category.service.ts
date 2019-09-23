c8b6e38749c63f39f9c73bc68ccfe07bca644abc
Joshua Tucker <thejltucker@gmail.com>
2019-09-23 13:52:59 -0400

    Category service functionality built out

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Category } from '../models/Category';
 
 const Api_Url = 'https://localhost:44383';
 
@@ -12,6 +13,22 @@ export class CategoryService {
 
   getCategories() {
     return this._http.get(`${Api_Url}/api/category`, { headers: this.getHeaders() });
+  }
+
+  getCategoryById(id) {
+    return this._http.get(`${Api_Url}/api/category/${id}`, { headers: this.getHeaders() });
+  }
+
+  createCategory(category: Category) {
+    return this._http.post(`${Api_Url}/api/category`, category, { headers: this.getHeaders() });
+  }
+
+  updateCategory(category: Category) {
+    return this._http.put(`${Api_Url}/api/category`, category, { headers: this.getHeaders() });
+  }
+
+  deleteCategory(id) {
+    return this._http.delete(`${Api_Url}/api/category/${id}`, {headers: this.getHeaders()});
   }
 
   private getHeaders(): HttpHeaders {
